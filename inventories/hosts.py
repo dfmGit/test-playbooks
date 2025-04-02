a7c55cd7d31bcc4e0d061e14fcbd27a3a03861cf
dfmGit <83708183+dfmGit@users.noreply.github.com>
2025-04-02 07:45:58 +0200

    Update hosts.py

--- inventories/hosts.py
+++ inventories/hosts.py
@@ -1,44 +1,57 @@
-#!usrbinenv python3
-import ipaddress
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
 
-# Ustawienie limitu hostów do wygenerowania dla danego zakresu (dla celów demonstracyjnych)
-host_limit = 10
+from datetime import datetime
 
-# Definicja grup i odpowiadających im zakresów IP
-# Dla zakresu 10.239. używamy podsieci 16, dla innych 24
-groups = {
-    szwalnia_dm [ipaddress.ip_network(10.10.21.024)],
-    szwalnia_re [ipaddress.ip_network(10.238.21.024)],
-    szwalnia_ol [ipaddress.ip_network(10.239.0.016)],
-    montownia [
-        ipaddress.ip_network(10.10.24.024),
-        ipaddress.ip_network(10.10.25.024)
-    ]
-}
+# This is a useful script to determine what happens on an inventory import
+# when the contents change. By using a timestamp, we should force the values
+# herein to change every time the sync is done.
+#
+# There are 2 examples
+#  - demonstrate new / old hosts by changing the hostname returned each time
+#         which is relevant to `overwrite`
+#  - demonstrate `overwrite_vars` by including 2 sub-examples
+#      - variable changes its name on every import
+#      - variable changes its value on every import
 
-def generate_hosts(network, limit)
-    
-    Generuje listę hostów z danego zakresu sieci, pomijając adres sieciowy i broadcast.
-    Zwraca nie więcej niż 'limit' hostów.
-    
-    hosts = list(network.hosts())
-    return hosts[limit]
 
-# Generowanie pliku inwentaryzacji w formacie INI
-inventory_lines = []
+time_val = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S.%f')
 
-for group, networks in groups.items()
-    inventory_lines.append(f[{group}])
-    for network in networks
-        hosts = generate_hosts(network, host_limit)
-        for host in hosts
-            inventory_lines.append(str(host))
-    inventory_lines.append()  # pusta linia oddzielająca grupy
+moover = "moover-{}".format(time_val)
 
-# Zapis do pliku lub wyświetlenie na ekranie
-inventory_text = n.join(inventory_lines)
-print(inventory_text)
-
-# Jeśli chcesz zapisać wynik do pliku, odkomentuj poniższe linie
-# with open(hosts.ini, w) as f
-#     f.write(inventory_text)
+print(json.dumps({
+    "_meta": {
+        "hostvars": {
+            "change_of_vars": {
+                "static_key": "host_dynamic_{}".format(time_val),
+                "dynamic_{}".format(time_val): "host_static_value"
+            },
+            moover: {
+                "static_var": "static_value"
+            }
+        }
+    },
+    "all": {
+        "vars": {
+            "static_inventory_key": "inventory_dynamic_{}".format(time_val),
+            "dynamic_{}".format(time_val): "inventory_static_value"
+        }
+    },
+    "group_with_moover": {
+        "hosts": ["change_of_vars", moover]
+    },
+    "group_with_vars": {
+        "hosts": ["change_of_vars"],
+        "vars": {
+            "static_group_key": "group_dynamic_{}".format(time_val),
+            "dynamic_group_{}".format(time_val): "group_static_value"
+        }
+    },
+    "ungrouped": {
+        "hosts": [
+            moover,
+            "change_of_vars"
+        ]
+    }
+}))
