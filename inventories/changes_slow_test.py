f2c20dfc50665a025d393a25b4ea5afb8bb1a42a
dfmGit <83708183+dfmGit@users.noreply.github.com>
2025-04-02 17:08:23 +0200

    Update changes_slow_test.py

--- inventories/changes_slow_test.py
+++ inventories/changes_slow_test.py
@@ -7,7 +7,7 @@ hosts = [f"10.10.24.{i}" for i in range(1, 255)] + [f"10.10.25.{i}" for i in ran
 # Struktura dynamicznego inwentarza Ansible
 inventory = {
     "all": {
-        "hosts": hosts,
+        "children": ["montownia"],
         "vars": {
             "ansible_user": "your_user",
             "ansible_ssh_private_key_file": "~/.ssh/id_rsa"
